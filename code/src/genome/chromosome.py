b62857cf377ccb06f4aa111efb7b45df924db0f8
vrettasm <vrettasm@gmail.com>
2024-05-29 12:16:57 +0200

    Initial base classes.

--- code/src/genome/chromosome.py
+++ code/src/genome/chromosome.py
@@ -1,3 +1,24 @@
+from gene import Gene
+from dataclasses import dataclass, field
+
+@dataclass(init=True, repr=True)
 class Chromosome(object):
-    pass
+
+    # Define the genome as a list of genes.
+    genome: list[Gene] = field(default_factory=list)
+
+    def is_valid(self):
+        """
+        Checks the validity of the whole chromosome, by
+        calling individually all genes is_valid method.
+
+        Additionally, it "double-checks" that all entries
+        in the genome are of type 'Gene'.
+
+        :return: True if ALL genes are valid, else False.
+        """
+        return all(isinstance(x, Gene) and x.is_valid
+                   for x in self.genome)
+    # _end_def_
+
 # _end_class_
--- code/src/genome/gene.py
+++ code/src/genome/gene.py
@@ -1,3 +1,39 @@
+from typing import Any
+from dataclasses import dataclass
+
+@dataclass(init=True, repr=True)
 class Gene(object):
-    pass
+
+    # Define the class members.
+    datum: Any = None
+    valid: bool = True
+
+    @property
+    def is_valid(self):
+        """
+        Accessor (getter) of the validity parameter.
+
+        :return: the valid value.
+        """
+        return self.valid
+    # _end_def_
+
+    @is_valid.setter
+    def is_valid(self, new_value):
+        """
+        Accessor (setter) of the validity flag.
+
+        :param new_value: (bool).
+        """
+        # Check for correct type.
+        if isinstance(new_value, bool):
+
+            # Update the flag value.
+            self.valid = new_value
+        else:
+            raise TypeError(f"{self.__class__.__name__}: "
+                            f"Validity flag should be bool: {type(new_value)}.")
+        # _end_if_
+    # _end_def_
+
 # _end_class_
