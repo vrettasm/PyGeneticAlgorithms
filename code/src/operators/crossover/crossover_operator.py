fb6ccf260f510fa2613b0a2e04f0e30cf153355b
vrettasm <vrettasm@gmail.com>
2024-06-03 12:45:15 +0200

    Update the base crossover operator.

--- code/src/operators/crossover/crossover_operator.py
+++ code/src/operators/crossover/crossover_operator.py
@@ -2,7 +2,7 @@ from code.src.operators.genetic_operator import GeneticOperator
 
 class CrossoverOperator(GeneticOperator):
 
-    def __init__(self, crossover_probability: float = 1.0):
+    def __init__(self, crossover_probability):
         """
         Construct a 'CrossoverOperator' object with a given
         probability value.
