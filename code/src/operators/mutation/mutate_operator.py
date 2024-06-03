575e3d179b37dbbd32fefa7710f5c22e5697fdb3
vrettasm <vrettasm@gmail.com>
2024-06-03 12:45:49 +0200

    Update the base mutation operator.

--- code/src/operators/mutation/mutate_operator.py
+++ code/src/operators/mutation/mutate_operator.py
@@ -2,15 +2,16 @@ from code.src.operators.genetic_operator import GeneticOperator
 
 class MutationOperator(GeneticOperator):
 
-    def __init__(self, mutation_probability: float = 0.001):
+    def __init__(self, mutation_probability: float):
         """
-        Construct a 'MutationOperator' object with a given
-        probability value.
+        Construct a 'MutationOperator' object with a
+        given probability value.
 
         :param mutation_probability: (float).
         """
 
-        # Call the super constructor with the provided probability value.
+        # Call the super constructor with the provided
+        # probability value.
         super().__init__(mutation_probability)
     # _end_def_
 
