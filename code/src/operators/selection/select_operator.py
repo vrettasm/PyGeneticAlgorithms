933eccf21caa4dba7e3cd77d8b98502f4f188651
vrettasm <vrettasm@gmail.com>
2024-06-04 14:01:46 +0200

    Added default constructor.

--- code/src/operators/selection/select_operator.py
+++ code/src/operators/selection/select_operator.py
@@ -1,5 +1,18 @@
 from src.operators.genetic_operator import GeneticOperator
 
 class SelectionOperator(GeneticOperator):
-    pass
+
+    def __init__(self, selection_probability: float):
+        """
+        Construct a 'SelectionOperator' object with a
+        given probability value.
+
+        :param selection_probability: (float).
+        """
+
+        # Call the super constructor with the provided
+        # probability value.
+        super().__init__(selection_probability)
+    # _end_def_
+
 # _end_class_
