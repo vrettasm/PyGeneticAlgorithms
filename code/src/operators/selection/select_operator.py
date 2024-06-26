3f5685d687d069235b26464046b0145d1dfa4e71
vrettasm <vrettasm@gmail.com>
2024-06-26 11:50:24 +0200

    Added 'abstract' method select in base class to remind the user that this method needs to be implemented accordingly.

--- code/src/operators/selection/select_operator.py
+++ code/src/operators/selection/select_operator.py
@@ -15,4 +15,16 @@ class SelectionOperator(GeneticOperator):
         super().__init__(selection_probability)
     # _end_def_
 
+    def select(self):
+        """
+        Abstract method that reminds the user that if they want to
+        create a Selection Class that inherits from here they should
+        implement a select method.
+
+        :return: Nothing but raising an error.
+        """
+        raise NotImplementedError(f"{self.__class__.__name__}: "
+                                  f"You should implement this method!")
+    # _end_def_
+
 # _end_class_
