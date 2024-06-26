ce135fd0185a990e799e9fc2261136ca192e672f
vrettasm <vrettasm@gmail.com>
2024-06-26 20:44:56 +0200

    RS version 1.

--- code/src/operators/selection/probability_selector.py
+++
@@ -1,5 +0,0 @@
-from src.operators.selection.select_operator import SelectionOperator
-
-class ProbabilitySelector(SelectionOperator):
-    pass
-# _end_class_
---
+++ code/src/operators/selection/random_selector.py
@@ -0,0 +1,38 @@
+from src.genome.chromosome import Chromosome
+from src.operators.selection.select_operator import SelectionOperator
+
+class RandomSelector(SelectionOperator):
+    """
+    Description:
+
+        Random Selector implements selection assuming that all member of the population have the same probability
+        to be selected as parents (effectively assuming a uniform probability).
+
+        It does not favour the fit individuals therefore the mixing will be very slow.
+    """
+
+    def __init__(self, select_probability: float = 1.0):
+        """
+        Construct a 'RandomSelector' object with a given probability value.
+
+        :param select_probability: (float).
+        """
+
+        # Call the super constructor with the provided
+        # probability value.
+        super().__init__(select_probability)
+    # _end_def_
+
+    def select(self, population: list[Chromosome]):
+        """
+        Select the new individuals from the population that will be passed on to the next genetic operations
+        of crossover and mutation to form the new population of solutions.
+
+        :param population:
+
+        :return: a new population (list of chromosomes).
+        """
+
+        # Return the (new) selected individuals.
+        return population[self.rng.choice(len(population), replace=True, shuffle=False)]
+# _end_class_
