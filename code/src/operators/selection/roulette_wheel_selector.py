f954358bb5e5691ca8f824368040ac418d2579d7
vrettasm <vrettasm@gmail.com>
2024-06-26 11:42:21 +0200

    RWS 1st version is done.

--- code/src/operators/selection/roulette_wheel_selector.py
+++ code/src/operators/selection/roulette_wheel_selector.py
@@ -1,5 +1,58 @@
+from math import fsum
+from src.genome.chromosome import Chromosome
 from src.operators.selection.select_operator import SelectionOperator
 
+
 class RouletteWheelSelector(SelectionOperator):
-    pass
+    """
+    Description:
+
+        Roulette Wheel Selector implements 'fitness proportional selection'.
+        Its member of the population is assigned a probability value that is
+        directly proportional to its fitness value (compared to the rest of
+        the population).
+
+        Individuals with higher fitness value are more likely to be selected
+        for parents when forming the new generation of individuals (offsprings).
+    """
+
+    def __init__(self, select_probability: float = 1.0):
+        """
+        Construct a 'RouletteWheelSelector' object with
+        a given probability value.
+
+        :param select_probability: (float).
+        """
+
+        # Call the super constructor with the provided
+        # probability value.
+        super().__init__(select_probability)
+    # _end_def_
+
+    def select(self, population: list[Chromosome]):
+        """
+        Select the new individuals from the population that will
+        be passed on to the next genetic operations of crossover
+        and mutation to form the new population of solutions.
+
+        :param population:
+
+        :return: a new population (list of chromosomes).
+        """
+
+        # Extract the fitness value of each chromosome making
+        # sure it is a positive value.
+        abs_fitness = [abs(p._fitness) for p in population]
+
+        # Calculate sum of all fitness.
+        sum_fitness = fsum(abs_fitness)
+
+        # Calculate the "selection probabilities", of each member
+        # in the population.
+        selection_probs = [f / sum_fitness for f in abs_fitness]
+
+        # Return the (new) selected individuals.
+        return population[self.rng.choice(len(population), p=selection_probs)]
+    # _end_def_
+
 # _end_class_
