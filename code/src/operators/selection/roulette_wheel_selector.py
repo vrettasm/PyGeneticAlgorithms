27dd136ce61d07ae3458b9c7f1988efa300e56ca
vrettasm <vrettasm@gmail.com>
2024-06-26 12:25:54 +0200

    Disable shuffling the new population.

--- code/src/operators/selection/roulette_wheel_selector.py
+++ code/src/operators/selection/roulette_wheel_selector.py
@@ -7,19 +7,16 @@ class RouletteWheelSelector(SelectionOperator):
     """
     Description:
 
-        Roulette Wheel Selector implements 'fitness proportional selection'.
-        Its member of the population is assigned a probability value that is
-        directly proportional to its fitness value (compared to the rest of
-        the population).
+        Roulette Wheel Selector implements 'fitness proportional selection'. Its member of the population is assigned a
+        probability value that is directly proportional to its fitness value (compared to the rest of the population).
 
-        Individuals with higher fitness value are more likely to be selected
-        for parents when forming the new generation of individuals (offsprings).
+        Individuals with higher fitness value are more likely to be selected for parents when forming the new generation
+        of individuals (offsprings).
     """
 
     def __init__(self, select_probability: float = 1.0):
         """
-        Construct a 'RouletteWheelSelector' object with
-        a given probability value.
+        Construct a 'RouletteWheelSelector' object with a given probability value.
 
         :param select_probability: (float).
         """
@@ -31,9 +28,8 @@ class RouletteWheelSelector(SelectionOperator):
 
     def select(self, population: list[Chromosome]):
         """
-        Select the new individuals from the population that will
-        be passed on to the next genetic operations of crossover
-        and mutation to form the new population of solutions.
+        Select the new individuals from the population that will be passed on to the next genetic operations of
+        crossover and mutation to form the new population of solutions.
 
         :param population:
 
@@ -52,7 +48,7 @@ class RouletteWheelSelector(SelectionOperator):
         selection_probs = [f / sum_fitness for f in abs_fitness]
 
         # Return the (new) selected individuals.
-        return population[self.rng.choice(len(population), p=selection_probs)]
+        return population[self.rng.choice(len(population), p=selection_probs, shuffle=False)]
     # _end_def_
 
 # _end_class_
