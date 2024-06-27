9659419e8a4602ab6acc8b1ff3888b26244188bd
vrettasm <vrettasm@gmail.com>
2024-06-27 11:45:54 +0200

    SUS version 1.

--- code/src/operators/selection/stochastic_universal_selector.py
+++ code/src/operators/selection/stochastic_universal_selector.py
@@ -1,5 +1,82 @@
+from math import fsum
+from numpy import cumsum
+from src.genome.chromosome import Chromosome
 from src.operators.selection.select_operator import SelectionOperator
 
 class StochasticUniversalSelector(SelectionOperator):
-    pass
+    """
+    Description:
+
+        Stochastic Universal Selector is an extension of fitness proportionate selection (i.e. Roulette Wheel Selection)
+        which exhibits no bias and minimal spread. Where RWS chooses several solutions from the population by repeated
+        random sampling, SUS uses a single random value to sample all the solutions by choosing them at evenly spaced
+        intervals. This gives weaker members of the population (according to their fitness) a chance to be chosen.
+    """
+
+    def __init__(self, select_probability: float = 1.0):
+        """
+        Construct a 'StochasticUniversalSelector' object with a given probability value.
+
+        :param select_probability: (float).
+        """
+
+        # Call the super constructor with the provided
+        # probability value.
+        super().__init__(select_probability)
+    # _end_def_
+
+    def select(self, population: list[Chromosome]):
+        """
+        Select the new individuals from the population that will be passed on to the next genetic
+        operations of crossover and mutation to form the new population of solutions.
+
+        :param population:
+
+        :return: a new population (list of chromosomes).
+        """
+
+        # Extract the fitness value of each chromosome making
+        # sure it is a positive value.
+        abs_fitness = [abs(p._fitness) for p in population]
+
+        # Get the size of the population.
+        N = len(population)
+
+        # Compute the distance between pointers.
+        dist_p = fsum(abs_fitness) / N
+
+        # Get a random number between 0 and dist_p.
+        start_0 = dist_p * self.rng.random()
+
+        # Pointers := [Start + i*P | i in [0..(N-1)]]
+        pointers = [start_0 + i*dist_p for i in range(0, N)]
+
+        # Create a list that will contain the new parents.
+        new_parents = []
+
+        # Get the list append method locally.
+        new_parents_append = new_parents.append
+
+        # Compute the cumulative sum of the fitness values.
+        cum_sum_fit = cumsum(abs_fitness)
+
+        # Collect the new parents.
+        for p in pointers:
+
+            # Reset the index to '0'.
+            i = 0
+
+            # Find the cumulative value smaller than 'p'.
+            while cum_sum_fit[i] < p:
+                i += 1
+            # _end_while_
+
+            # Add the individual at position 'i' in the new parents pool.
+            new_parents_append(population[i])
+        # _end_for_
+
+        # Return the (new) selected individuals.
+        return new_parents
+    # _end_def_
+
 # _end_class_
