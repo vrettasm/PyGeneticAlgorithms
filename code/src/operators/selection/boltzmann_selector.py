2889935254f1eae91ca5dfa8422fad4f0fcbfb7d
vrettasm <vrettasm@gmail.com>
2024-06-27 14:27:31 +0200

    Removed 2 selectors.

--- code/src/operators/selection/boltzmann_selector.py
+++
@@ -1,5 +0,0 @@
-from src.operators.selection.select_operator import SelectionOperator
-
-class BoltzmannSelector(SelectionOperator):
-    pass
-# _end_class_
--- code/src/operators/selection/monte_carlo_selector.py
+++
@@ -1,5 +0,0 @@
-from src.operators.selection.select_operator import SelectionOperator
-
-class MonteCarloSelector(SelectionOperator):
-    pass
-# _end_class_
