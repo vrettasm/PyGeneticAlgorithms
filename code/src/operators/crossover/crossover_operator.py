9b6243961450042d56e94843adaf02a3724c04f5
vrettasm <vrettasm@gmail.com>
2024-06-28 11:07:58 +0200

    Added abstract methods.

--- code/src/operators/crossover/crossover_operator.py
+++ code/src/operators/crossover/crossover_operator.py
@@ -1,3 +1,4 @@
+from src.genome.chromosome import Chromosome
 from src.operators.genetic_operator import GeneticOperator
 
 class CrossoverOperator(GeneticOperator):
@@ -13,5 +14,20 @@ class CrossoverOperator(GeneticOperator):
         # Call the super constructor with the provided probability value.
         super().__init__(crossover_probability)
     # _end_def_
+
+    def crossover(self, parent1: Chromosome, parent2: Chromosome):
+        """
+        Abstract method that "reminds" the user that if they want to
+        create a Crossover Class that inherits from here they should
+        implement a crossover method.
+
+        :param parent1: (Chromosome).
+
+        :param parent2: (Chromosome).
+
+        :return: Nothing but raising an error.
+        """
+        raise NotImplementedError(f"{self.__class__.__name__}: "
+                                  f"You should implement this method!")
 
 # _end_class_
--- code/src/operators/mutation/mutate_operator.py
+++ code/src/operators/mutation/mutate_operator.py
@@ -1,3 +1,4 @@
+from src.genome.chromosome import Chromosome
 from src.operators.genetic_operator import GeneticOperator
 
 class MutationOperator(GeneticOperator):
@@ -13,6 +14,20 @@ class MutationOperator(GeneticOperator):
         # Call the super constructor with the provided
         # probability value.
         super().__init__(mutation_probability)
+    # _end_def_
+
+    def mutate(self, parent: Chromosome):
+        """
+        Abstract method that "reminds" the user that if they want to
+        create a Mutation Class that inherits from here they should
+        implement a mutate method.
+
+        :param parent: the chromosome to be mutated.
+
+        :return: Nothing but raising an error.
+        """
+        raise NotImplementedError(f"{self.__class__.__name__}: "
+                                  f"You should implement this method!")
     # _end_def_
 
 # _end_class_
