70d1bfffa294c81411167ce288f951e9a19fa31c
vrettasm <vrettasm@gmail.com>
2024-07-12 13:21:35 +0200

    Added auxiliary container class.

--- code/src/engines/auxiliary.py
+++ code/src/engines/auxiliary.py
@@ -1,9 +1,9 @@
-import numpy as np
+from dataclasses import dataclass, field
 from src.genome.chromosome import Chromosome
 
 
 # Public interface.
-__all__ = ["apply_corrections"]
+__all__ = ["apply_corrections", "SubPopulation"]
 
 def apply_corrections(input_population: list[Chromosome]) -> int:
     """
@@ -47,3 +47,80 @@ def apply_corrections(input_population: list[Chromosome]) -> int:
     # Return the total number of corrected genes.
     return corrections_counter
 # _end_def_
+
+@dataclass(init=True, repr=True)
+class SubPopulation(object):
+    """
+    Auxiliary class container used in the IslandModelGA
+    to hold all the subpopulations (one on each island).
+    """
+
+    # SubPopulation ID.
+    pop_id: int
+
+    # List of chromosomes.
+    population: list = field(default_factory=list[Chromosome])
+
+    @property
+    def id(self) -> int:
+        """
+        Accessor (getter) of the id parameter.
+
+        :return: the id value.
+        """
+        return int(self.pop_id)
+    # _end_def_
+
+    @id.setter
+    def id(self, new_value: int):
+        """
+        Accessor (setter) of the id parameter.
+
+        :param new_value: (int).
+        """
+        # Check for correct type.
+        if isinstance(new_value, int):
+
+            # Update the id value.
+            self.pop_id = new_value
+        else:
+            raise TypeError(f"{self.__class__.__name__}: "
+                            f"SubPopulation id should be <int>: {type(new_value)}.")
+        # _end_if_
+    # _end_def_
+
+    def __len__(self) -> int:
+        """
+        Accessor of the total length of the population.
+
+        :return: the length (int) of the population.
+        """
+        return len(self.population)
+    # _end_def_
+
+    def __getitem__(self, index: int):
+        return self.population[index]
+    # _end_def_
+
+    def __setitem__(self, index: int, item: Chromosome):
+        self.population[index] = item
+    # _end_def_
+
+    def __contains__(self, item: Chromosome) -> bool:
+        """
+        Check for membership.
+
+        :param item: an input Chromosome that we want to check
+        if it exists in general population.
+
+        :return: true if the 'item' belongs in the population.
+        """
+        return item in self.population
+    # _end_if_
+
+# _end_class_
+
+
+s1 = SubPopulation(1, ["q", "b"])
+
+print(s1.id)
