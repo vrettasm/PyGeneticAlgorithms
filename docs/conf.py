7fbc987a0d8561830faeff4b8a2269704e4038e5
vrettasm <vrettasm@gmail.com>
2024-08-26 13:57:37 +0200

    Updated path.

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../code'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -34,8 +34,6 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
