341e85d4ea5c8168277cbb5ab55eccf8a8ba3ef4
vrettasm <vrettasm@gmail.com>
2024-11-26 12:59:18 +0100

    Update conf file.

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,7 @@ release = "01.60.50"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
-    "python_docs_theme"
+    "sphinx.ext.napoleon"
 ]
 
 templates_path = ["_templates"]
@@ -48,5 +47,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = "alabaster", "sphinx_rtd_theme"
-html_theme = "python_docs_theme"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
