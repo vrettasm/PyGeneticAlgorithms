7a2a0cad3ed3fdfbea80c05a23efa8d2007d35a0
vrettasm <vrettasm@gmail.com>
2025-11-18 11:41:33 +0100

    Fix typo in multi_point_crossover.py

--- tests/test_multi_point_crossover.py
+++ tests/test_multi_point_crossover.py
@@ -1,7 +1,7 @@
 import unittest
 from pygenalgo.genome.gene import Gene
 from pygenalgo.genome.chromosome import Chromosome
-from pygenalgo.operators.crossover.mutli_point_crossover import MultiPointCrossover
+from pygenalgo.operators.crossover.multi_point_crossover import MultiPointCrossover
 
 
 class TestMultiPointCrossover(unittest.TestCase):
