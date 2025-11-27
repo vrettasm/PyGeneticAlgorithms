38661ec8327a1be319bcba6943c6725eb44842a1
vrettasm <vrettasm@gmail.com>
2025-11-27 19:10:46 +0100

    Added basic information in __init__.py

---
+++ pygenalgo/__init__.py
@@ -0,0 +1,14 @@
+#                     GNU GENERAL PUBLIC LICENSE
+#                        Version 3, 29 June 2007
+#
+#  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+#  Everyone is permitted to copy and distribute verbatim copies
+#  of this license document, but changing it is not allowed.
+#
+
+# Basic Information.
+__author__ = "Michalis Vrettas, PhD"
+
+__email__ = "michail.vrettas@gmail.com"
+
+__version__ = "1.8.0"
