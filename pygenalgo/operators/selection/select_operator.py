8783e8662ea3cd876cd7d9c6b5c3ccdb6d6a22e3
michailvrettas <vrettasm@gmail.com>
2026-01-15 15:50:28 +0100

    Added a new static method that ensures the fitness values of the chromosomes are positive. This uses a shift transformation, where the minimum value is shifted to 1, along with every other fitness value shifted by the same amount.

--- pygenalgo/operators/selection/select_operator.py
+++ pygenalgo/operators/selection/select_operator.py
@@ -23,6 +23,37 @@ class SelectionOperator(GeneticOperator):
         super().__init__(selection_probability)
     # _end_def_
 
+    @staticmethod
+    def ensure_positive_fitness(population: list[Chromosome]) -> list[float]:
+        """
+        Ensures that the fitness value of each chromosome is a positive number.
+
+        This is useful because some of the selection methods require a positive
+        fitness to operate. Also in minimization problems the fitness values are
+        negated, therefore by using this transformation the methods that require
+        positive values are guaranteed to work.
+
+        :param population: (list) of chromosomes.
+
+        :return: (list) of positive fitness values.
+        """
+        # Extract the fitness value of each chromosome.
+        all_fitness = [p.fitness for p in population]
+
+        # If there are negative values we perform a shift
+        # transformation where all the values are shifted
+        # so that the minimum fitness is going to be one.
+        if any(fit_value < 0.0 for fit_value in all_fitness):
+            # Compute the shift value.
+            shift_value = fabs(min(all_fitness)) + 1.0
+
+            # Shift all fitness values so that the minimum is '1'.
+            all_fitness = [f + shift_value for f in all_fitness]
+        # _end_if_
+
+        return all_fitness
+    # _end_def_
+
     def select(self, population: list[Chromosome]) -> list[Chromosome]:
         """
         Abstract method that "reminds" the user that if they want to
