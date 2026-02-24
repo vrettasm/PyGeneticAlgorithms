dd247fc3309b0030f4182d37a39561397cb53484
vrettasm <vrettasm@gmail.com>
2026-02-24 11:51:02 +0100

    Update order_crossover operator to use two crossover points (one for each parent).

--- pygenalgo/operators/crossover/order_crossover.py
+++ pygenalgo/operators/crossover/order_crossover.py
@@ -38,19 +38,20 @@ class OrderCrossover(CrossoverOperator):
         # changes.
         if (parent1 != parent2) and self.is_operator_applicable():
 
-            # Select randomly the crossover point from [1, M-1].
-            locus = self.rng.integers(1, high=len(parent1)-1, dtype=int)
+            # Select randomly two crossover points from [1, M-1].
+            loc1, loc2 = self.rng.integers(1, high=len(parent1)-1,
+                                           size=2, dtype=int)
 
             # Create auxiliary Sets for faster membership check.
-            used_in_parent1 = set(parent1.genome[:locus])
-            used_in_parent2 = set(parent2.genome[:locus])
+            used_in_parent1 = set(parent1.genome[:loc1])
+            used_in_parent2 = set(parent2.genome[:loc2])
 
             # Construct 1st offspring genome list at locus.
-            genome_1 = [gene.clone() for gene in parent1.genome[:locus] +
+            genome_1 = [gene.clone() for gene in parent1.genome[:loc1] +
                         [x for x in parent2 if x not in used_in_parent1]]
 
             # Construct 2nd offspring genome list at locus.
-            genome_2 = [gene.clone() for gene in parent2.genome[:locus] +
+            genome_2 = [gene.clone() for gene in parent2.genome[:loc2] +
                         [y for y in parent1 if y not in used_in_parent2]]
 
             # Create two NEW offsprings.
