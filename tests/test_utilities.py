d989aa0919ea295f93ee98ff62d031aea26cb948
michail vrettas <vrettasm@gmail.com>
2026-07-22 15:49:16 +0200

    Added test for np_pareto_front.

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,7 +1,8 @@
 import unittest
 import numpy as np
 
-from pygenalgo.utils.utilities import two_indices_fast
+from pygenalgo.utils.utilities import (np_pareto_front,
+                                       two_indices_fast)
 
 
 class TestUtilities(unittest.TestCase):
@@ -38,6 +39,31 @@ class TestUtilities(unittest.TestCase):
 
             # They must be different.
             self.assertNotEqual(i, j)
+    # _end_def_
+
+    def test_np_pareto_front(self) -> None:
+        """
+        Test the functionality of np_pareto_from_index.
+
+        :return:
+        """
+        # Select randomly n_points.
+        n_points = self.rng.integers(low=10, high=100, dtype=int)
+
+        # Select randomly n_dim.
+        n_dim = self.rng.integers(low=2, high=10, dtype=int)
+
+        # Generate random points.
+        points = np.random.randn(n_points, n_dim)
+
+        # Extract the pareto points.
+        p_front = np_pareto_front(points, return_index=False)
+
+        # Extract the indices.
+        i_front = np_pareto_front(points, return_index=True)
+
+        for k, l in zip(i_front, p_front):
+            self.assertEqual(0.0, np.sum(l - points[k]))
     # _end_def_
 
 # _end_class_
