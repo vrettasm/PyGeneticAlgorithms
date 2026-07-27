5d2f9b44615877ef7ae492a820346dfbf3ee30f3
michail vrettas <vrettasm@gmail.com>
2026-07-27 20:28:35 +0200

    Reformat code.

--- pygenalgo/engines/multi_objective_ga.py
+++ pygenalgo/engines/multi_objective_ga.py
@@ -37,8 +37,8 @@ class MultiObjectiveGA(GenericGA):
 
     # pylint: disable=arguments-differ
     def run(self, epochs: int = 100, elitism: bool = True, correction: bool = False,
-            parallel: bool = False, adapt_probs: bool = False,
-            shuffle: bool = True, f_max_eval: Optional[int] = None, verbose: bool = False) -> None:
+            parallel: bool = False, adapt_probs: bool = False, shuffle: bool = True,
+            f_max_eval: Optional[int] = None, verbose: bool = False) -> None:
         """
         Main method of the StandardGA class, that implements the evolutionary routine.
 
