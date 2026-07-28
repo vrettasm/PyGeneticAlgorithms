a20f8e88a414118125e29c8193633e0cfddebf0a
michail vrettas <vrettasm@gmail.com>
2026-07-28 10:14:45 +0200

    1st working version of ParetoTournamentSelector.

--- pygenalgo/operators/selection/pareto_tournament_selector.py
+++ pygenalgo/operators/selection/pareto_tournament_selector.py
@@ -7,7 +7,7 @@ from pygenalgo.operators.genetic_operator import increase_counter
 from pygenalgo.operators.selection.select_operator import SelectionOperator
 
 
-class ParetoFrontSelector(SelectionOperator):
+class ParetoTournamentSelector(SelectionOperator):
     """
     Description:
 
@@ -16,9 +16,9 @@ class ParetoFrontSelector(SelectionOperator):
     """
 
     def __init__(self, select_probability: float = 1.0,
-                 n_contestants: int = 2) -> None:
+                 n_contestants: int = 5) -> None:
         """
-        Construct a 'ParetoFrontSelector' object with a given probability value.
+        Construct a 'ParetoTournamentSelector' object with a given probability value.
 
         :param select_probability: (float) in [0, 1].
 
@@ -28,7 +28,7 @@ class ParetoFrontSelector(SelectionOperator):
         super().__init__(selection_probability=select_probability)
 
         # Set the value of the contestants in the placeholder _items.
-        self._items: int = max(2, int(n_contestants))
+        self._items: int = max(5, int(n_contestants))
     # _end_def_
 
     @increase_counter
@@ -38,8 +38,8 @@ class ParetoFrontSelector(SelectionOperator):
         to the next genetic operations of crossover and mutation to form the new
         population of solutions.
         """
-        # Properly build a 2D array from
-        # the fitness tuples (objectives).
+        # Build a 2D array from the fitness
+        # tuples (optimization objectives).
         fitness_array: NDArray = np.array([
             p.fitness for p in population
         ], dtype=float)
@@ -50,11 +50,12 @@ class ParetoFrontSelector(SelectionOperator):
         # Local copy of random choice.
         choose_randomly = self.rng.choice
 
-        # Number of contestants.
-        n_contestants = self._items
+        # Local number of contestants.
+        n_contestants: int = self._items
 
-        # Select the contestants for the tournaments.
+        # Select the contestants in one call.
         contestants: NDArray = np.array([
+            # Set 'replace=False' to avoid duplicates.
             choose_randomly(n_size, size=n_contestants,
                             replace=False, shuffle=False)
             for _ in range(n_size)
@@ -62,82 +63,16 @@ class ParetoFrontSelector(SelectionOperator):
 
         # Select the new indices via Tournament selection.
         chosen: list[int] = [
+            # We need to 'dereference' the returned index of
+            # pareto back to the population index (via row).
             row[np_pareto_front_index(fitness_array[row])[0]]
             for row in contestants
         ]
 
-        return [population[k] for k in chosen]
-    # _end_def_
-
-    @increase_counter
-    def select_22(self, population: list[Chromosome]) -> list[Chromosome]:
-        """
-        Select the individuals, from the input population that will be passed on
-        to the next genetic operations of crossover and mutation to form the new
-        population of solutions.
-        """
-        # Properly build a 2D array from
-        # the fitness tuples (objectives).
-        fitness_array: NDArray = np.array([
-            p.fitness for p in population
-        ], dtype=float)
-
-        # Total size of the population.
-        n_size: int = len(population)
-
-        # Extract original index positions directly.
-        # Note: The fitness values have already been
-        # set for maximization : so here the default
-        # mode = "max" is assumed.
-        pareto_idx: NDArray = np_pareto_front_index(fitness_array)
-
-        # Remaining size.
-        r_size: int = n_size - pareto_idx.size
-
-        # Check if the remaining size is positive.
-        if r_size > 0:
-            # Fast extraction of the remaining indices.
-            remaining_idx: NDArray = np.setdiff1d(np.arange(n_size),
-                                                  pareto_idx,
-                                                  assume_unique=True)
-            # Local copy of random choice.
-            choose_randomly = self.rng.choice
-
-            # Number of contestants.
-            n_contestants = self._items
-
-            # Check if we have enough remaining indices.
-            if remaining_idx.size < n_contestants:
-
-                # Select all the remaining indices.
-                contestants: NDArray = np.tile(remaining_idx,
-                                               (r_size, 1))
-            else:
-
-                # Select the contestants for the tournaments.
-                contestants: NDArray = np.array([
-                    choose_randomly(remaining_idx, size=n_contestants,
-                                    replace=False, shuffle=False)
-                    for _ in range(r_size)
-                ], dtype=int)
-
-            # Select the extras via Tournament selection.
-            extras = [
-                row[np_pareto_front_index(fitness_array[row])[0]]
-                for row in contestants
-            ]
-
-            # Combined fast concatenation.
-            chosen: NDArray = np.concatenate((pareto_idx, extras))
-        else:
-            # Getting here is highly unlikely.
-            chosen: NDArray = pareto_idx
-        # _end_if_
-
-        # Ensure the items are shuffled.
-        self.rng.shuffle(chosen)
-
-        return [population[k] for k in chosen]
+        return [
+            # Ensure 'k' is passed as integer.
+            population[int(k)] for k in chosen
+        ]
     # _end_def_
 
 # _end_class_
