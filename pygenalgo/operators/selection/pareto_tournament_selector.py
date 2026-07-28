05541ed3a89eef32937f0d3c5d049b3297ad79e2
michail vrettas <vrettasm@gmail.com>
2026-07-28 11:44:39 +0200

    Update docstrings.

--- pygenalgo/operators/selection/pareto_tournament_selector.py
+++ pygenalgo/operators/selection/pareto_tournament_selector.py
@@ -13,8 +13,7 @@ class ParetoTournamentSelector(SelectionOperator):
         This selector follows the same logic of the TournamentSelector
         but instead of choosing the one with the optimal fitness value
         it passes the contestants to the pareto_front_index function,
-        and selects the first one that is on the Pareto front.
-
+        and selects the first one that is on the Pareto frontier.
     """
 
     def __init__(self, select_probability: float = 1.0,
