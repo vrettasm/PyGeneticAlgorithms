5bd66e60277c5b938019c97336c4060ee9a5e0a2
michail vrettas <vrettasm@gmail.com>
2026-07-28 12:46:35 +0200

    Update GaussianMutator to use different values for different gene positions.

--- pygenalgo/operators/mutation/gaussian_mutator.py
+++ pygenalgo/operators/mutation/gaussian_mutator.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import numpy as np
+from numpy.typing import NDArray, ArrayLike
 
 from pygenalgo.utils.utilities import clamp
 from pygenalgo.genome.chromosome import Chromosome
@@ -13,8 +14,10 @@ class GaussianMutator(MutationOperator):
         and perturbing it with a Gaussian random value to the current gene value.
     """
 
-    def __init__(self, mutate_probability: float = 0.1, sigma: float = 1.0,
-                 lower_val: Optional[float] = None, upper_val: Optional[float] = None) -> None:
+    def __init__(self, mutate_probability: float = 0.1,
+                 sigma: ArrayLike | float = 1.0,
+                 lower_lim: ArrayLike = None,
+                 upper_lim: ArrayLike = None) -> None:
         """
         Construct a 'GaussianMutator' object with a given probability value.
 
@@ -30,35 +33,37 @@ class GaussianMutator(MutationOperator):
         super().__init__(mutation_probability=mutate_probability)
 
         # Ensure sigma parameter is float.
-        sigma = float(sigma)
+        sigma: NDArray = np.asarray(sigma, dtype=float)
 
         # Ensure standard deviation is positive.
-        if sigma <= 0.0:
+        if np.any(sigma <= 0.0):
             raise ValueError(f"{self.__class__.__name__}: "
                              f"Standard deviation must be positive.")
         # _end_if_
 
-        # Ensure that both lower and upper limits are provided.
-        if lower_val is None or upper_val is None:
+        # Check if the lower and upper bounds are set.
+        if (lower_lim is None) or (upper_lim is None):
             raise ValueError(f"{self.__class__.__name__}: "
                              f"Lower or Upper limits are missing.")
         # _end_if_
 
-        # Ensure lower_val parameter is float.
-        lower_val: float = float(lower_val)
+        # Make sure the limits are numpy arrays.
+        lower_lim: NDArray = np.asarray(lower_lim, dtype=float)
+        upper_lim: NDArray = np.asarray(upper_lim, dtype=float)
 
-        # Ensure upper_val parameter is float.
-        upper_val: float = float(upper_val)
-
-        # Ensure the order is correct.
-        if upper_val <= lower_val:
+        # Check if there is a size mismatch.
+        if lower_lim.size != upper_lim.size:
             raise ValueError(f"{self.__class__.__name__}: "
-                             f"The limit values are incorrect.")
-        # _end_if_
+                             f"Lower and Upper limits sizes do not match.")
+
+        # Check if the boundaries are set correctly.
+        if np.any(upper_lim <= lower_lim):
+            raise ValueError(f"{self.__class__.__name__}: "
+                             f"Lower and Upper limits are set incorrectly.")
 
         # Assign variables to the _items placeholder.
         self._items: tuple[float, ...] = (
-            sigma, lower_val, upper_val
+            sigma, lower_lim, upper_lim
         )
     # _end_def_
 
@@ -78,20 +83,27 @@ class GaussianMutator(MutationOperator):
             # Get the size of the chromosome.
             n_genes: int = len(individual)
 
-            # Extract the values from the placeholder variable.
+            # Extract the variables from the placeholder.
             sigma, xl, xu = self._items
 
+            # If sigma is scalar use the
+            # value for all gene positions.
+            if np.isscalar(sigma):
+                sigma *= np.ones_like(xl)
+
             # Select a random position in the genome.
-            i = self.rng.integers(n_genes, dtype=int)
+            idx = self.rng.integers(n_genes, dtype=int)
 
             # Get the old value of the Gene.
-            old_value = individual[i].value
+            old_value: float = individual[idx].value
 
-            # Calculate the new Gene value by sampling from N(value, sigma),
-            # ensuring it stays within limits.
-            individual[i].value = clamp(self.rng.normal(loc=old_value, scale=sigma),
-                                        xl, xu)
-            # Set the fitness to NaN.
+            # Calculate the new Gene value by sampling from N(mu,sigma).
+            new_value: float = self.rng.normal(loc=old_value, scale=sigma[idx])
+
+            # Ensure it stays within limits.
+            individual[idx].value = clamp(new_value, xl[idx], xu[idx])
+
+            # Set the fitness to None.
             individual.invalidate_fitness()
 
             # Increase the mutator counter.
