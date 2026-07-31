845e8eccec152b2dceaba1f8574afa85932a398f
michail vrettas <vrettasm@gmail.com>
2026-07-31 15:23:31 +0200

    Fixes previous merge error.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf8") as readme_file:
 
 setup(
     name='pygenalgo',
-    version='2.0.0',
+    version='2.0.1',
     author='Michalis Vrettas, PhD',
     author_email='michail.vrettas@gmail.com',
     description='Genetic Algorithms toolbox in Python3',
