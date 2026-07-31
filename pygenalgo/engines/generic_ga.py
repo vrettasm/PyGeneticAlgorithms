a9faa07bdbeebc2e4fe67b48c82be134ba0504af
michail vrettas <vrettasm@gmail.com>
2026-07-31 15:01:32 +0200

    Added back missing functions: print_operator_stats and correct_genome from the merge.

--- pygenalgo/engines/generic_ga.py
+++ pygenalgo/engines/generic_ga.py
@@ -531,6 +531,70 @@ class GenericGA:
         return fitness_values, found_solution
     # _end_def_
 
+    def correct_genome(self, input_population: list[Chromosome]) -> bool:
+        """
+        Applies the correction mechanism to the input population
+        (the changes in the population are passed by reference).
+
+        :param input_population: list of Chromosomes.
+
+        :return: True if changes were made, False otherwise.
+        """
+        # Return flag.
+        has_been_corrected: bool = False
+
+        # Apply the "correct_chromosomes" function.
+        total_corrections, f_counts = correct_chromosomes(input_population,
+                                                          self.fitness_func)
+        # If corrections were made we will
+        # need to update the f_evals value.
+        if total_corrections > 0:
+            # Update the function evaluation counter.
+            self._f_evals += f_counts
+
+            # Log the corrections.
+            logger.debug(
+                "> %d correction(s) took place at epoch: %d",
+                total_corrections, self._iteration
+            )
+
+            # Enable the flag.
+            has_been_corrected = True
+
+        return has_been_corrected
+    # _end_def_
+
+    def print_operator_stats(self) -> None:
+        """
+        Print the genetic operators stats.
+
+        :return: None.
+        """
+        # First print the selection operator.
+        print(self._select_op)
+
+        # Second print the crossover operator.
+        print(self._crossx_op)
+
+        # Check if we used the MetaCrossover.
+        if isinstance(self._crossx_op, MetaCrossover):
+            # Call internally all operators.
+            for op in self._crossx_op.items:
+                print(op)
+        # _end_if_
+
+        # Lastly print the mutation operator.
+        print(self._mutate_op)
+
+        # Check if we used the MetaMutator.
+        if isinstance(self._mutate_op, MetaMutator):
+            # Call internally all operators.
+            for op in self._mutate_op.items:
+                print(op)
+            # _end_for_
+
+    # _end_def_
+
     def run(self, *args, **kwargs) -> None:
         """
         Main method of the Generic GA class
