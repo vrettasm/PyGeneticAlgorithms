401d11d0e797e2943bca3ab5190928f0b3991854
michail vrettas <vrettasm@gmail.com>
2026-08-03 09:08:50 +0200

    Fix typo.

--- pygenalgo/engines/__init__.py
+++ pygenalgo/engines/__init__.py
@@ -3,7 +3,7 @@ import logging
 # Create a logger object for the engine module.
 logger = logging.getLogger("engines_logger")
 
-# Setup a basic configuration.
+# Set up a basic configuration.
 logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s",
                     datefmt="%m/%d/%Y %H:%M:%S", level=logging.INFO)
 
