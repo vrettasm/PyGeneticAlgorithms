eecac993e29f9f1f745b77d4fd6341a5145bb570
michail vrettas <vrettasm@gmail.com>
2026-08-25 08:54:40 +0200

    Update imports.

--- pygenalgo/engines/island_model_ga.py
+++ pygenalgo/engines/island_model_ga.py
@@ -15,6 +15,7 @@ from pygenalgo.genome.chromosome import Chromosome
 from pygenalgo.utils.auxiliary import (SubPopulation,
                                        average_hamming_distance)
 # Custom PyGenaAlgo code.
+from pygenalgo.operators.island_manager import IslandManager
 from pygenalgo.engines.generic_ga import GenericGA, RunConfig
 from pygenalgo.operators.island_operator import IslandOperator
 from pygenalgo.operators.migration.meta_migration import MetaMigration
