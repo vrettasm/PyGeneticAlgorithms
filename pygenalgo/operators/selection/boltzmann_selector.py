e78e959cd9579fa578155e3c649509a4900e76ee
michail vrettas <vrettasm@gmail.com>
2026-09-09 15:22:01 +0200

    Update BoltzmannSelector() with the "safety option".

--- pygenalgo/operators/selection/boltzmann_selector.py
+++ pygenalgo/operators/selection/boltzmann_selector.py
@@ -73,12 +73,9 @@ class BoltzmannSelector(SelectionOperator):
         # fall back to uniform random selection so every
         # individual has equal chance.
         if isclose(sum_fitness, 0.0):
-            # Select chromosomes randomly with equal probability.
-            safe_index = self.rng.choice(pop_size, size=pop_size,
-                                         replace=True, shuffle=False)
-
-            # Return the new parents to a list.
-            return [population[i] for i in safe_index]
+            # Return the new parents.
+            return self.safety_option(population,
+                                      pop_size)
         # _end_if_
 
         # Calculate the selection probabilities of each member
