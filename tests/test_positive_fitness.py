df6ff770bdac774c522d1f5dba02a7cf552a2986
michail vrettas <vrettasm@gmail.com>
2026-09-25 15:36:47 +0200

    Remove default argument from construction of Chromosome.

--- tests/test_positive_fitness.py
+++ tests/test_positive_fitness.py
@@ -141,7 +141,7 @@ class TestPositiveFitness(unittest.TestCase):
             pass
 
         population = [
-            Chromosome(Gene(0, func), f_value, True)
+            Chromosome(Gene(0, func), f_value)
             for f_value in expected
         ]
 
